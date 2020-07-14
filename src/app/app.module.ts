d6b90f5798413c93767d6635828f6fa8d2d63573
snippet99 <morten.salomonsen@gmail.com>
2020-07-15 01:03:56 +0200

    Lagt til ts.lib, ng2 adsense og oppdatert kode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { registerLocaleData } from '@angular/common';
 import { LoanAppComponent } from './loanapp.component';
 import { Term } from './term';
 import localeNb from '@angular/common/locales/nb';
+import { AdsenseModule } from 'ng2-adsense';
 
 registerLocaleData(localeNb, 'nb');
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,
+  AdsenseModule.forRoot({
+      adClient: 'ca-pub-4020578640432504',
+      adSlot: 7259870550,
+    }) ],
   declarations: [ LoanAppComponent ],
   providers: [ { provide: LOCALE_ID, useValue: 'nb' } ],
   bootstrap:    [ LoanAppComponent ]
